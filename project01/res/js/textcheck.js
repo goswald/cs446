3e915eb900bd62e90da7413815f947cb540a025d
Gilliane Oswald <goswald@mines.edu>
2012-01-16 11:06:18 -0700

    Got the 'back-end' and 'front-end' linked and working properly.

--- project01/res/js/textcheck.js
+++ project01/res/js/textcheck.js
@@ -1,7 +1,6 @@
 function checkText() {
-    var string1=document.inputForm.howYouDoin.value
+    var string1=document.forms["inputForm"]["howYouDoin"].value;
     if (string1.length==0) {
-        alert("Surely, you must feel something!")
-        document.inputForm.howYouDoin.focus()
+        alert("Surely, you must feel *something*!");
     }
 }
