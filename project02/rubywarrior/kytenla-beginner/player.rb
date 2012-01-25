c1f9b2e106b0041b9539cfe07ec48400994d810f
Gilliane Oswald <goswald@mines.edu>
2012-01-24 18:16:55 -0700

    Added a method to feel what's in front of the warrior and attack if there is something ahead.  (Hurray for passing level 2. :D)

--- project02/rubywarrior/kytenla-beginner/player.rb
+++ project02/rubywarrior/kytenla-beginner/player.rb
@@ -1,7 +1,10 @@
 class Player
   def play_turn(warrior)
-          warrior.walk!
-    
-          # add your code here
+    if warrior.feel.empty? then
+        warrior.walk!  
+    else
+        warrior.attack!
+    end
+    # add your code here
   end
 end
