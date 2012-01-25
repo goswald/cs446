72e77232e661cb9aa384fc05be86bd8a1fb5d80e
Gilliane Oswald <goswald@mines.edu>
2012-01-25 00:13:50 -0700

    FINALLY found out an algorithm for Level 6.  Now, for level 7...

--- project02/rubywarrior/kytenla-beginner/player.rb
+++ project02/rubywarrior/kytenla-beginner/player.rb
@@ -1,20 +1,31 @@
 class Player
   def play_turn(warrior)
-    if warrior.feel.empty? then
-        if warrior.health < 20 and warrior.health >= @health then
-            warrior.rest!
+    b = warrior.feel:backward
+    if b.wall? and warrior.health < 20
+        warrior.rest!
+    elsif warrior.feel.empty?
+        if warrior.health > 10
+            warrior.walk!
         else
-            warrior.walk!  
-        end
+            go_back_and_heal(warrior)
+        end       
     else
         if warrior.feel.captive? then
             warrior.rescue!
+        elsif warrior.health < 5 then
+            go_back_and_heal(warrior)
         else
             warrior.attack!
-        end
+        end 
     end
     @health = warrior.health
-
-    # add your code here
+  end
+  def go_back_and_heal(warrior)
+    b = warrior.feel:backward
+    if b.empty?
+        warrior.walk!:backward
+    elsif b.captive? then
+            warrior.rescue!:backward
+    end
   end
 end
