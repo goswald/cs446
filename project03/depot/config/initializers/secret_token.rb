60245419ff081410d2a3e504a8d99b19045eab8c
Gilliane Oswald <moone@tj.lcarsos.com>
2012-02-12 09:38:35 -0700

    Finished setting up the scaffolding of the project, up through the 'Making it look prettier' subtask.

---
+++ project03/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '232dc6e23b1af03cb8eb5d3c62db92764563c6dd66de7754aef2d000d2649ff7ba21cc57bfc4a803362183f9a2d5befc868d5f7b23ae362acdc0c46115e060d9'
