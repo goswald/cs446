668d30e8fabdf464ef91a7b3d78cd7315e12ae65
Gilliane Oswald <goswald@mines.edu>
2012-02-13 19:42:37 -0700

    Wow, I've done a lot since my last commit...  + CSS for Store/Products views  + Code and tests for products in the database (Are they valid?  Do they conflict at all?)  + Formatting and tests for prices (Is it greater than -bash.01?  Does it show up as a monetary amount, or just a decimal?  + Initial Cart scaffolding and linkage to products, as well as tests for those
    The next iteration will fix the multiple-item display, at least.

--- project03/depot/test/unit/product_test.rb
+++ project03/depot/test/unit/product_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+    fixtures :products
     test "product attributes must not be empty" do
-        product = Product.net
+        product = Product.new
         assert product.invalid?
         assert product.errors[:title].any?
         assert product.errors[:description].any?
@@ -20,5 +21,24 @@ class ProductTest < ActiveSupport::TestCase
         product.price = 1
         assert product.valid?
     end
-end
+    def new_product(image_url)
+        Product.new(title: "My Book Title", description: "yyy", price: 1, image_url: image_url)
+    end
+    test "image url" do
+        ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/x/fred.gif }
+        bad = %w{ fred.doc fred.gif/more fred.gif.more }
+        ok.each do |name|
+            assert new_product(name).valid?, "#{name} shouldn't be inavlid"
+        end
 
+        bad.each do |name|
+            assert new_product(name).invalid? "#{name} shouldn't be valid"
+        end
+    end
+
+    test "product is not valid without a unique title - i18n" do
+        product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
+        assert !product.save
+        assert_equal I18n.translate( 'activerecord.errors.messages.taken'), product.errors[:title].join( '; ')
+    end
+end
