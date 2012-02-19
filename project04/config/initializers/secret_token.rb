cbf86e3ff5cd857da72aef459cc26cb0cd6a9dc5
Gilliane Oswald <goswald@mines.edu>
2012-02-19 10:35:13 -0700

    Initial generation of articl.es scaffolding.

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = 'e25cd2b8dac7bfde6d8ad9d58d259f56b246c5d6357422933cbb284c1f11fae39195e088c00b68d0d4b65dbfa62978f412a8085bcd14d3a77c09e3468ee735b0'
