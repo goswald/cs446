6fcbb4780699cec2ccd3aadcf6d138f79be035bc
Gilliane Oswald <goswald@mines.edu>
2012-02-22 17:20:11 -0700

    Initial (re-)setup, beginning CSS work, and DB populaton.

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+    validates :title, :author, :body, presence: true
+    validates_exclusion_of :author, :in => "pat", :message => "I'm sorry, but %{value} is not an accepted author name." 
 end
--- project04/config/initializers/secret_token.rb
+++ project04/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Articles::Application.config.secret_token = 'e25cd2b8dac7bfde6d8ad9d58d259f56b246c5d6357422933cbb284c1f11fae39195e088c00b68d0d4b65dbfa62978f412a8085bcd14d3a77c09e3468ee735b0'
+Articles::Application.config.secret_token = '49a0ab5f818d0986c8528812e48c5e594b4dcde9a670a5c630c0ddd6e8fbd8073fb19569b3311096421bbbcbeece3e5085d6c38cd529ef1f8ed5bdd935627227'
