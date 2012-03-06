9a6db96304453d451e29335df184ac1c0eaf1899
Gilliane Oswald <goswald@mines.edu>
2012-03-06 16:07:25 -0700

    Changed everything to haml, and changed the CSS/SCSS to SASS.  Also, got paginate to work; that's yet to be styled.

--- project05/app/models/article.rb
+++ project05/app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
     validates :title, :author, :body, presence: true
     validates_format_of :author, :with => %r{^((?!pat).*$)}i, message: "name isn't accepted"
+    
+end
+class Article
+    self.per_page = 10
 end
