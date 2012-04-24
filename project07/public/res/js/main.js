b0e40e992636107c356b215590a0995792715ad0
Gilliane Oswald <goswald@mines.edu>
2012-04-23 22:42:37 -0600

    Almost done, but why, when your score is 10, does highScores.sort() treat 10 like 1?

--- project07/public/res/js/main.js
+++ project07/public/res/js/main.js
@@ -1,12 +1,88 @@
 var guessesLeft = 10;
 var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
+var number = Math.floor(Math.random()*101);
+var theDate = new Date();
+var dateTimeVariable = (theDate.getFullYear() + theDate.getMonth() + theDate.getDate() + theDate.getHours() + theDate.getMinutes() + theDate.getSeconds());
 
 $(function() {
   updateScore(guessesLeft);
   populateHighScores(highScores);
 });
 
+function addUser(score, name) {
+  highScores.push([score, name]);
+  highScores.sort();
+  highScores.reverse();
+  populateHighScores(highScores);
+}
+
+function playAgain() {
+  number = Math.floor(Math.random()*101);
+  guessesLeft = 10;
+  updateScore(guessesLeft);
+}
+
+function fadeIn() {
+  $("h1").fadeIn("fast");
+  $("p").fadeIn("fast");
+  $("#guessTheNumber").fadeIn("fast");
+  $("h2").fadeIn("fast");
+}
+
+function fadeOut() {
+  $("h1").fadeOut("fast");
+  $("p").fadeOut("fast");
+  $("#guessTheNumber").fadeOut("fast");
+  $("h2").fadeOut("fast");
+}
+
+function guessMe() {
+  var guesstimate = $("#guess").val();
+  if (guessesLeft > 0) {
+      if (guesstimate < number) {
+        alert("TOO LOW.");
+        guessesLeft--;
+        updateScore(guessesLeft);
+        $("#guess").val("");
+      }
+      else if (guesstimate > number) {
+        alert("TOO HIGH.");
+        guessesLeft--;
+        updateScore(guessesLeft);
+        $("#guess").val("");
+      } 
+      else {
+        fadeOut();
+        var username = prompt("YOU WIN.  PLEASE GIVE ME YOUR NAME:", "SURVIVOR-" + dateTimeVariable);
+        addUser(guessesLeft, username);
+        var y = confirm("PLAY AGAIN?  (PRESS OK.)");
+        if (y) {
+          fadeIn();
+          playAgain();
+        }
+        else {
+          alert("ENJOY YOUR REMAINING DAYS...");
+          window.close();
+        }
+      } 
+  }
+}
+
+function youLost() {
+  fadeOut();
+  var x = confirm("YOU LOST.  PRESS OK TO PLAY AGAIN.");
+  if (x) {
+    fadeIn();
+    playAgain();
+  }
+  else {
+    alert("ENJOY DEATH'S ICY EMBRACE...");
+    window.close();
+  }
+}
+    
 function populateHighScores(scores) {
+//  $("div#highScores").text("");
   for (var i = 0; i < scores.length; ++i) {
     $('div#highScores').append("<p>" + scores[i][0] + " " + scores[i][1] + "</p>");
   }
@@ -17,24 +93,13 @@ function updateScore(score) {
 }
 
 $(document).ready(function() {
-  var number = Math.floor(Math.random()*101);
-  alert("The number is " + number);
+  alert("NUMBER IS " + number);
   $("#btnGuess").click(function() {
-    var guesstimate = $("#guess").val();
-    if (guesstimate < number) {
-      alert("TOO LOW.");
-      guessesLeft--;
-      updateScore(guessesLeft);
-      $("#guess").val("");
-    }
-    else if (guesstimate > number) {
-      alert("TOO HIGH.");
-      guessesLeft--;
-      updateScore(guessesLeft);
-      $("#guess").val("");
-    }
-    else {
-      alert("YOU WIN.");
+      guessMe();   
+  });
+  $("#guess").keydown(function(event) {
+    if(event.keyCode == 13) {
+      guessMe();
     }
   });
 });
